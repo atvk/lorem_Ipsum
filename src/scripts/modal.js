75cc9d27976309d2450f12a0b86285469b3e5e3d
Anton Steblovskiy <steblovskiyanton@yandex.ru>
2025-02-04 23:52:39 +0300

    update layout

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,20 +1,26 @@
+
+const buttonConnection = document.querySelector(".connection-button");
+const popupOpen = document.querySelector(".popup_type_order");
+const popupClose = document.querySelector("popup__close")
+const popup = document.querySelectorAll(".popup");
+
+const buttonSearch = document.querySelector(".search");
+const mainContainer = document.querySelector(".main");
+
 function handleEscape(e) {
   if (e.key === "Escape") {
     const openedPopup = document.querySelector(".popup_is-opened");
     closePopup(openedPopup);
   }
 }
-
 function openPopup(popup) {
   popup.classList.add("popup_is-opened");
   document.addEventListener("keydown", handleEscape);
 }
-
 function closePopup(popup) {
   popup.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", handleEscape);
 }
-
 function setCloseByClickListeners(popups) {
   popups.forEach((popup) => {
     popup.addEventListener("mousedown", (e) => {
@@ -28,15 +34,12 @@ function setCloseByClickListeners(popups) {
   });
 }
 
-const buttonConnection = document.querySelector(".connection-button");
-const popupOpen = document.querySelector(".popup_type_order");
-const popupClose = document.querySelector("popup__close")
-const popups = document.querySelectorAll(".popup");
-
-buttonConnection.addEventListener("click", () => {
-openPopup(popupOpen);
-});
-
-setCloseByClickListeners(popups);
+buttonConnection.addEventListener("click", () => {openPopup(popupOpen)});
+setCloseByClickListeners(popup);
 
 
+function toggleMainVisibility(conteiner) {
+  conteiner.style.visibility = conteiner.style.visibility === 'hidden' ? 'visible' : 'hidden';
+}
+
+buttonSearch.addEventListener("click", () => {toggleMainVisibility(mainContainer)});
